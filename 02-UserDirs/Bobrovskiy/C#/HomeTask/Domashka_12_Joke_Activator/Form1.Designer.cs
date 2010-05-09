edef2dd8cc8ec23df8d4a7f0c5f5d718553598ee
valnet13 <valnet13@e6463aac-b588-11de-a66b-f3ad5cb19abf>
2010-05-09 10:02:12 +0000

    Генератор Жартів

---
+++ 02-UserDirs/Bobrovskiy/C#/HomeTask/Domashka_12_Joke_Activator/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace lesson_12_winform
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.TrayNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ShowJokeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.ShowHideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SelectFileButton = new System.Windows.Forms.Button();
+            this.FileNameTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FunnyJouksLabel = new System.Windows.Forms.Label();
+            this.NotFunnyJoaksLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TrayNotifyIcon
+            // 
+            this.TrayNotifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.TrayNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayNotifyIcon.Icon")));
+            this.TrayNotifyIcon.Text = "Шутник";
+            this.TrayNotifyIcon.Visible = true;
+            this.TrayNotifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.BackColor = System.Drawing.SystemColors.Control;
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ShowJokeToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.ShowHideToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(187, 82);
+            // 
+            // ShowJokeToolStripMenuItem
+            // 
+            this.ShowJokeToolStripMenuItem.Name = "ShowJokeToolStripMenuItem";
+            this.ShowJokeToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.ShowJokeToolStripMenuItem.Text = "Показать шутку";
+            this.ShowJokeToolStripMenuItem.Click += new System.EventHandler(this.ShowJokeToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(183, 6);
+            // 
+            // ShowHideToolStripMenuItem
+            // 
+            this.ShowHideToolStripMenuItem.Name = "ShowHideToolStripMenuItem";
+            this.ShowHideToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.ShowHideToolStripMenuItem.Text = "Показать/Спрятать";
+            this.ShowHideToolStripMenuItem.Click += new System.EventHandler(this.ShowHideToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(183, 6);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.ExitToolStripMenuItem.Text = "&Выход";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Файл с шутками :";
+            // 
+            // SelectFileButton
+            // 
+            this.SelectFileButton.Location = new System.Drawing.Point(315, 21);
+            this.SelectFileButton.Name = "SelectFileButton";
+            this.SelectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.SelectFileButton.TabIndex = 2;
+            this.SelectFileButton.Text = "Выбрать ...";
+            this.SelectFileButton.UseVisualStyleBackColor = true;
+            this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // FileNameTextBox
+            // 
+            this.FileNameTextBox.Location = new System.Drawing.Point(6, 23);
+            this.FileNameTextBox.MaxLength = 200;
+            this.FileNameTextBox.Name = "FileNameTextBox";
+            this.FileNameTextBox.ReadOnly = true;
+            this.FileNameTextBox.Size = new System.Drawing.Size(303, 20);
+            this.FileNameTextBox.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FunnyJouksLabel);
+            this.groupBox1.Controls.Add(this.NotFunnyJoaksLabel);
+            this.groupBox1.Location = new System.Drawing.Point(2, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(398, 83);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Статистика шуток";
+            // 
+            // FunnyJouksLabel
+            // 
+            this.FunnyJouksLabel.AutoSize = true;
+            this.FunnyJouksLabel.Location = new System.Drawing.Point(3, 24);
+            this.FunnyJouksLabel.Name = "FunnyJouksLabel";
+            this.FunnyJouksLabel.Size = new System.Drawing.Size(65, 13);
+            this.FunnyJouksLabel.TabIndex = 3;
+            this.FunnyJouksLabel.Text = "Смешные : ";
+            // 
+            // NotFunnyJoaksLabel
+            // 
+            this.NotFunnyJoaksLabel.AutoSize = true;
+            this.NotFunnyJoaksLabel.Location = new System.Drawing.Point(3, 56);
+            this.NotFunnyJoaksLabel.Name = "NotFunnyJoaksLabel";
+            this.NotFunnyJoaksLabel.Size = new System.Drawing.Size(49, 13);
+            this.NotFunnyJoaksLabel.TabIndex = 2;
+            this.NotFunnyJoaksLabel.Text = "Отстой :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.FileNameTextBox);
+            this.groupBox2.Controls.Add(this.SelectFileButton);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(2, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(398, 61);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(402, 161);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Шутник";
+            this.ClientSizeChanged += new System.EventHandler(this.MainForm_ClientSizeChanged);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TrayNotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem ShowJokeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem ShowHideToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.TextBox FileNameTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label FunnyJouksLabel;
+        private System.Windows.Forms.Label NotFunnyJoaksLabel;
+    }
+}
+
