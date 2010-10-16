9aa5a6ec6f4aeca5731c65a27ad8f02bf3cdc606
valnet13 <valnet13@e6463aac-b588-11de-a66b-f3ad5cb19abf>
2010-10-16 16:02:31 +0000

    Error Log Interface for home task. Just modify and use.

---
+++ 02-UserDirs/Bobrovskiy/Network_programming/ClientServerJournalingSystem/ErrorLogger_Client/ILog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ErrorLogger_Client
+{
+    public interface ILogAppender
+    {
+        void Debug(string format, string message);
+        void Debug(string message);
+
+        void Info(string format, string message);
+        void Info(string message);
+
+        void Fatal(string format, string message);
+        void Fatal(string message);
+    }
+
+    public static class Logger
+    {
+        private static List<ILogAppender> logAppenders;
+
+        static Logger()
+        {
+            Intitalize();
+        }
+
+        static void Intitalize()
+        {
+            logAppenders = new List<ILogAppender>();
+        }
+
+        public static void AddAppender(ILogAppender appender)
+        {
+            logAppenders.Add(appender);
+        }
+
+        public static void Debug(string format, string message)
+        {
+            foreach (ILogAppender appender in logAppenders)
+            {
+                appender.Debug(format, message);
+            }
+        }
+
+        public static void Debug(string message)
+        {
+            Debug(message, string.Empty);
+        }
+
+        public static void Info(string format, string message) { }
+        public static void Info(string message) { }
+
+        public static void Fatal(string format, string message) { }
+        public static void Fatal(string message) { }
+    }
+}
